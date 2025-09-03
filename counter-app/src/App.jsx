0e72b0ff677d4ad638488c088e7ce2fc58566ca7
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-09-03 18:05:19 +0600

    sumit bhai er redux counter demo understanding done

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { useState } from "react";
 import Counter from "./components/Counter";
+import Stats from "./components/Stats";
 
 const App = () => {
+  const initialCounters = [
+    {
+      id: 1,
+      value: 0,
+    },
+    {
+      id: 2,
+      value: 0,
+    },
+  ];
+
+  const [counters, setCounters] = useState(initialCounters);
+
+  const handleIncrement = (counterId) => {
+    const updatedCounters = counters.map((counter) => {
+      if (counter.id === counterId) {
+        return {
+          ...counter,
+          value: counter.value + 1,
+        };
+      }
+      return counter;
+    });
+
+    setCounters(updatedCounters);
+  };
+
+  const handleDecrement = (counterId) => {
+    const updatedCounters = counters.map((counter) => {
+      if (counter.id === counterId) {
+        return {
+          ...counter,
+          value: counter.value - 1,
+        };
+      }
+      return counter;
+    });
+    setCounters(updatedCounters);
+  };
+
+  const totalCount = counters.reduce((acc, curr) => acc + curr.value, 0);
+
   return (
     <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
       <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
@@ -9,8 +52,15 @@ const App = () => {
       </h1>
 
       <div className="max-w-md mx-auto mt-10 space-y-5">
-        <Counter />
-        <Counter />
+        {counters.map((counter) => (
+          <Counter
+            count={counter.value}
+            onIncrement={() => handleIncrement(counter.id)}
+            onDecrement={() => handleDecrement(counter.id)}
+            key={counter.id}
+          />
+        ))}
+        <Stats count={totalCount} />
       </div>
     </div>
   );
--- counter-app/src/components/Counter.jsx
+++ counter-app/src/components/Counter.jsx
@@ -1,31 +1,20 @@
-import React, { useState } from 'react'
-import Count from './Count';
-import Button from './Button';
+import React, { useState } from "react";
+import Count from "./Count";
+import Button from "./Button";
 
-const Counter = () => {
-    const [count, setCount] = useState(0);
-    const handleIncrement = () =>{
-        setCount((prevCount) => prevCount + 1)
-    }
+const Counter = ({ count, onIncrement, onDecrement }) => {
+  return (
+    <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+      <Count count={count} />
 
-    const handleDecrement = ()=> {
-        setCount((prevCount)=> prevCount - 1)
-    }
-    
-    return (
-    <div className='p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow'>
-
-        <Count count={count} />
-
-        <div className='flex space-x-3'>
-
-
-            <Button handler={handleIncrement} >Increment</Button>
-            <Button handler={handleDecrement} type="danger">Decrement</Button>
-
-        </div>
+      <div className="flex space-x-3">
+        <Button handler={onIncrement}>Increment</Button>
+        <Button handler={onDecrement} type="danger">
+          Decrement
+        </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
