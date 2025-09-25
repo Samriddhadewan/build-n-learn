1282441d3d68cf80fe18fdcecad248c5ed031153
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-09-25 12:15:57 +0600

    hourly temperature component is done

--- KLIMATE/src/components/HourlyTemprature.tsx
+++ KLIMATE/src/components/HourlyTemprature.tsx
@@ -1,6 +1,6 @@
 import type { ForeCastData } from "@/api/types"
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
-import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from "recharts"
+import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
 import { format } from "date-fns"
 interface HourlyTempratureProps {
     data: ForeCastData;
@@ -30,13 +30,41 @@ return (
                         <YAxis stroke="#888888" fontSize={12} tickLine={false} axisLine={false} tickFormatter={(value)=> `${value}°`} />
                         
                         {/* tooltip  */}
+                        <Tooltip 
+                        content={({active,payload})=> {
+                            if(active && payload && payload.length){
+                                return (
+                                    <div className="rounded-lg border bg-background p-2 shadow-sm">
+                                        <div className="grid grid-cols-2 gap-2">
+                                            <div className="flex flex-col">
+                                                <span className="text-[0.70rem] uppercase text-muted-foreground">Temperature </span>
+                                                <span className="font-bold">{payload[0].value}°</span>
+                                            </div>
+                                            <div className="flex flex-col">
+                                                <span className="text-[0.70rem] uppercase text-muted-foreground">Feels Like </span>
+                                                <span className="font-bold">{payload[1].value}°</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        } }
+                        />
 
                         <Line 
                         type="monotone"
                         dataKey="temp"
                         stroke="#2563eb"
                         strokeWidth={2}
-                        dot={true}
+                        dot={false}
+                        />
+                        <Line 
+                        type="monotone"
+                        dataKey="feels_like"
+                        stroke="#64748b"
+                        strokeWidth={2}
+                        dot={false}
+                        strokeDasharray="5 5"
                         />
 
 
