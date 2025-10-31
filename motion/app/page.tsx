1c026be2a661c0a1a5287c601e23b281b53bebde
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-11-01 02:09:23 +0600

    (class1): raw css done

--- motion/app/page.tsx
+++ motion/app/page.tsx
@@ -5,10 +5,18 @@ import Navbar from '@/components/navbar';
 const Home = () => {
   return (
     <div className='layout'>
+      <div className='layout-lines-container'>
+        <div className='left-line' />
+        <div className='right-line' />
+      </div>
       <Container>
         <Navbar />
         <Hero />
       </Container>
+      <div className='hero-img-container'>
+        <div className='horizontal-line' />
+        <img src="/hero-ui-v5.webp" alt="" className='hero-img' />
+      </div>
     </div>
   )
 }
